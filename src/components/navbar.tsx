9ac58e7f94c66f24d9e9dd385f518aeead7f6924
Koh Uehara <refluster@gmail.com>
2022-10-01 22:27:43 -0700

    nab

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
     return (
-        <div className="navbar">
-            <div>Koh Uehara</div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+            <Toolbar>
+                <Box sx={{ flexGrow: 1 }}>Koh Uehara</Box>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+            </AppBar>
+      </Box>
     )
 };
 
