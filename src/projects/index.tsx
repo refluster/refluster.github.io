5b0916b2ae849f93c1722026cf957949c3a275fb
Koh Uehara <refluster@gmail.com>
2022-10-03 14:04:04 -0700

    layout

---
+++ src/projects/index.tsx
@@ -0,0 +1,29 @@
+import lumixGh5 from './lumix/lumix-gh5.png';
+import lumixA100 from './lumix/lumix-a100.png';
+
+export const Projects = [
+    {
+        title: 'Lumix',
+        subtitle: 'Digital still camera',
+        description: 'World first 4K movie suported camera.',
+        url: 'https://na.panasonic.com/us/lumix',
+        articles: [
+            'https://news.panasonic.com/global/press/en160920-5',
+            'https://www.digitalcameraworld.com/news/camera-rumors/6',
+        ],
+        images: [
+            lumixGh5,
+            lumixA100,
+        ]
+    }, {
+        title: "OND'U",
+        subtitle: '',
+        description: '',
+        url: 'https://tech.panasonic.com/jp/ondu/',
+        articles: [
+        ],
+        images: [
+
+        ]
+    }
+];
