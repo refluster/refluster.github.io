b59577a670246bf94e0343559f55f82430e0d61e
Koh Uehara <refluster@gmail.com>
2022-10-08 01:34:30 -0700

    unkai

--- src/projects/index.tsx
+++ src/projects/index.tsx
@@ -4,6 +4,10 @@ import lumixGM from './lumix/lumix-gm.jpeg';
 
 import onduApp from './ondu/ondu_app.png';
 
+import unkai from './unkai/unkai.jpg';
+import unkaiSunset from './unkai/unkai_sunset.jpg';
+import unkaiTablet from './unkai/unkai_tablet.jpg';
+
 export type Project = {
     title: string,
     subtitle: string,
@@ -30,13 +34,26 @@ export const Projects: Project[] = [
         ]
     }, {
         title: "OND'U",
-        subtitle: '',
+        subtitle: 'Health management software for compamnies, communities.',
         description: '',
         url: 'https://tech.panasonic.com/jp/ondu/',
         articles: [
         ],
         images: [
             onduApp,
+        ]
+    }, {
+        title: "Unkai",
+        subtitle: '',
+        description: 'Indoor green system for healing by nature',
+        url: 'https://refluster.github.io/schememono/unkai.html',
+        articles: [
+            'https://kyoto.theterminal.jp/events/schememonox%EF%BD%8B%EF%BD%99%EF%BD%8F%EF%BD%94%EF%BD%8F/'
+        ],
+        images: [
+            unkai,
+            unkaiSunset,
+            unkaiTablet,
         ]
     },
 ];
